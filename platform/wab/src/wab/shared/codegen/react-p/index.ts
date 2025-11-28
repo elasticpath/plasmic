170e7d45980e0cc43ec2a393b1632302ee3d6f7c
Robert Field <robertfield@quadrimular.com>
2025-11-28 13:54:42 +0000

    feat: add custom loader for plasmic image component

--- platform/wab/src/wab/shared/codegen/react-p/index.ts
+++ platform/wab/src/wab/shared/codegen/react-p/index.ts
@@ -1564,7 +1564,20 @@ export function serializeTplTagBase(
         }
       });
       if (ctx.exportOpts.imageOpts.scheme === "cdn") {
-        attrs["loader"] = jsLiteral("plasmic");
+        attrs["loader"] = `{
+    supportsUrl: (src) => {
+      return (src.startsWith("http") || /^([a-f0-9]{32})\\..{1,16}$/i.test(src)) && !(src.endsWith(".svg") || src.startsWith("data:image/svg"));
+    },
+    transformUrl: (opts) => {
+      const params = [
+        \`src=\${encodeURIComponent(opts.src)}\`,
+        opts.width ? \`w=\${opts.width}\` : undefined,
+        \`q=\${opts.quality ?? 75}\`,
+        opts.format ? \`f=\${opts.format}\` : undefined,
+      ].filter((x) => !!x);
+      return \`${DEVFLAGS.imgOptimizerHost}/img-optimizer/v1/img?\${params.join("&")}\`;
+    }
+  }`;
       }
     }
   }
