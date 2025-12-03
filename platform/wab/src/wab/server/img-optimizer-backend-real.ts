bf4c05c9e0d347f90118bd1609db16e7643d8fae
Robert Field <robertfield@quadrimular.com>
2025-12-03 14:22:40 +0000

    feat: add ensure db for image service (#68)

--- platform/wab/src/wab/server/img-optimizer-backend-real.ts
+++ platform/wab/src/wab/server/img-optimizer-backend-real.ts
@@ -2,15 +2,18 @@
 // the top so that we know if we are running production and want newrelic.
 import { addImgOptimizerRoutes, createApp } from "@/wab/server/AppServer";
 import { Config } from "@/wab/server/config";
+import { ensureDbConnections } from "@/wab/server/db/DbCon";
 import { runExpressApp, setupServerCli } from "@/wab/server/server-common";
 import "core-js";
 
 async function runImgOptimizerServer(config: Config) {
+  await ensureDbConnections(config.databaseUri);
+  
   const { app } = await createApp("img-optimizer", config, addImgOptimizerRoutes);
   return runExpressApp(app);
 }
 
 export async function imgOptimizerBackendMain() {
-  const { opts, config } = setupServerCli();
+  const { config } = setupServerCli();
   await runImgOptimizerServer(config);
 }
