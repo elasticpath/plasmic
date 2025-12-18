a38bcbe59562621b45a83ccd713ad0651d9efe66
Andy Lin <andy.lin@elasticpath.com>
2025-12-17 22:50:22 -0800

    feat: add handler to authenticate using cookie

--- platform/wab/src/wab/server/auth/custom-passport-cfg.ts
+++ platform/wab/src/wab/server/auth/custom-passport-cfg.ts
@@ -1,6 +1,38 @@
 import { Config } from "@/wab/server/config";
 import { DbMgr } from "@/wab/server/db/DbMgr";
+import passport from "passport";
+import { asyncToCallback } from "@/wab/shared/common";
+import { superDbMgr } from "@/wab/server/routes/util";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 
 export async function setupCustomPassport(dbMgr: DbMgr, config: Config) {
-  // Do nothing
+
+  // JWT for provisioning
+  passport.use(
+    "provision-jwt",
+    new JwtStrategy(
+      {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey:
+          "-----BEGIN PUBLIC KEY-----\n" +
+          "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEvAnj5ENp4CVKULbrSPD36WZyeOZV\n" +
+          "+J0SMo6743PmcvvZkjmm6SXLmV0Dv8HqOXDA1c7YkuRFp7QueT5RZEz4oQ==\n" +
+          "-----END PUBLIC KEY-----",
+        algorithms: ["ES256"],
+        passReqToCallback: true,
+      },
+      (req, jwt_payload, done) => {
+        asyncToCallback(done, async () => {
+          const mgr = superDbMgr(req);
+          const user = await mgr.tryGetUserByEmail(jwt_payload.sub);
+
+          if (!user) {
+            return false;
+          }
+
+          return user;
+        });
+      }
+    )
+  );
 }
