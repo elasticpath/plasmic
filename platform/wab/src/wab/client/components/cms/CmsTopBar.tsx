a5e7039926103eeb6cb1edc61bbb297909901be2
Robert Field <robertfield@quadrimular.com>
2026-01-29 16:12:41 +0000

    fix: use branded logo on CMS route
    Update CmsTopBar to use the brand configuration from appConfig
    instead of hardcoded Plasmic icons, matching the studio UI behavior.

--- platform/wab/src/wab/client/components/cms/CmsTopBar.tsx
+++ platform/wab/src/wab/client/components/cms/CmsTopBar.tsx
@@ -3,12 +3,14 @@ import {
   useCmsDatabase,
   useMutateDatabase,
 } from "@/wab/client/components/cms/cms-contexts";
-import { useApi } from "@/wab/client/contexts/AppContexts";
+import { PublicLink } from "@/wab/client/components/PublicLink";
+import { useApi, useAppCtx } from "@/wab/client/contexts/AppContexts";
 import {
   DefaultCmsTopBarProps,
   PlasmicCmsTopBar,
 } from "@/wab/client/plasmic/plasmic_kit_cms/PlasmicCmsTopBar";
 import { APP_ROUTES } from "@/wab/shared/route/app-routes";
+import { Tooltip } from "antd";
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
 import * as React from "react";
 
@@ -18,7 +20,11 @@ function CmsTopBar_(props: CmsTopBarProps, ref: HTMLElementRefOf<"div">) {
   const match = useRRouteMatch(APP_ROUTES.cmsRoot)!;
   const database = useCmsDatabase(match?.params.databaseId);
   const api = useApi();
+  const appCtx = useAppCtx();
   const mutateDatabase = useMutateDatabase();
+
+  const brand = appCtx.appConfig.brands?.[""];
+
   return (
     <PlasmicCmsTopBar
       root={{ ref }}
@@ -29,6 +35,22 @@ function CmsTopBar_(props: CmsTopBarProps, ref: HTMLElementRefOf<"div">) {
           await api.updateCmsDatabase(database!.id, { name: newName });
           await mutateDatabase(database!.id);
         },
+      }}
+      link={{
+        render: (linkProps) => (
+          <Tooltip title={brand?.logoTooltip ?? "Back to dashboard"}>
+            <PublicLink
+              {...linkProps}
+              href={brand?.logoHref ?? "/"}
+            >
+              {brand?.logoImgSrc ? (
+                <img src={brand.logoImgSrc} style={{ maxHeight: 40 }} />
+              ) : (
+                linkProps.children
+              )}
+            </PublicLink>
+          </Tooltip>
+        ),
       }}
       {...props}
     />
