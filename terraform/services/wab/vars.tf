cad49fe9cf18c80b5b1df61a445ae429a7193c77
James Willis <5223069+jameswillis99@users.noreply.github.com>
2026-02-17 16:54:58 +0000

    Increase default memory for wab to 2048 MB

--- terraform/services/wab/vars.tf
+++ terraform/services/wab/vars.tf
@@ -33,7 +33,7 @@ variable "wab_cpu" {
 variable "wab_memory" {
   type        = number
   description = "Task memory in MB"
-  default     = 1024
+  default     = 2048
 }
 
 variable "wab_desired_count" {
