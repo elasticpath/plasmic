1b9adb2a9c426d3cb6c4d03f399009001d76d130
Robert Field <robertfield@quadrimular.com>
2026-02-20 13:35:31 +0000

    fix: replace Plasmic with Visual Builder in dashboard page titles

--- platform/wab/src/wab/client/components/dashboard/page-utils.tsx
+++ platform/wab/src/wab/client/components/dashboard/page-utils.tsx
@@ -5,7 +5,7 @@ export function documentTitle(title: string) {
   return (
     // @ts-ignore
     <Helmet>
-      <title>{title ? `${title} - Plasmic` : `Plasmic`}</title>
+      <title>{title ? `${title} - Visual Builder` : `Visual Builder`}</title>
     </Helmet>
   );
 }
